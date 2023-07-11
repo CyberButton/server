a16634173d2486a6e9b7f8b79659a877f3c1e537
CyberButton <87766623307Kk>
2023-07-11 22:03:40 +0600

    omg i am kaming back-on-js

---
+++ controllers/controller.js
@@ -0,0 +1,26 @@
+
+
+export async function getQuestions(req, res) {
+    res.json("questions api get request")
+}
+
+export async function postQuestions(req, res) {
+    res.json("questions api POST request")
+}
+
+export async function deleteQuestions(req, res) {
+    res.json("questions api DELETE request")
+}
+
+//result
+export async function getResults(req, res) {
+    res.json("RESULTS api get request")
+}
+
+export async function postResults(req, res) {
+    res.json("RESULTS api POST request")
+}
+
+export async function deleteResults(req, res) {
+    res.json("RESULTS api DELETE request")
+}
---
+++ router/route.js
@@ -0,0 +1,18 @@
+import { Router } from "express"
+import * as controller from '../controllers/controller.js'
+const router = Router()
+
+//router.get('/questions', controller.getQuestions)
+//router.post("/questions", controller.insertQuestions)
+
+router.route('/questions')
+    .get(controller.getQuestions)
+    .post(controller.postQuestions)
+    .delete(controller.deleteQuestions)
+
+router.route('/results')
+    .get(controller.getResults)
+    .post(controller.postResults)
+    .delete(controller.deleteResults)
+
+export default router
