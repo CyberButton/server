64eab4949bcc1c498f61f98a8b6d7390059d15e1
CyberButton <87766623307Kk>
2023-07-13 11:23:16 +0600

    prepopulate-db

--- controllers/controller.js
+++ controllers/controller.js
@@ -13,13 +13,55 @@ export async function getQuestions(req, res) {
 export async function postQuestions(req, res) {
     try {
         Questions.insertMany({ 
-            questions : [0], 
-            answers : [1]
-    }//,
-    //     function(err, data) {
-    //         res.json({msg : "data saved succesfully"})
-    //     }
-    )
+            questions : [
+                {
+                    id: 1,
+                    question : "Javascript is an _______ language",
+                    options : [
+                        'Object-Oriented',
+                        'Object-Based',
+                        'Procedural',
+                    ]
+                },
+                {
+                    id: 2,
+                    question : "Following methods can be used to display data in some form using Javascript",
+                    options : [
+                        'document.write()',
+                        'console.log()',
+                        'window.alert()',
+                    ]
+                },
+                {
+                    id: 3,
+                    question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+                    options : [
+                        'Boolean',
+                        'Undefined',
+                        'Object',
+                    ]
+                },
+                {
+                    id: 4,
+                    question : "What does the toString() method return?",
+                    options : [
+                        'Return Object',
+                        'Return String',
+                        'Return Integer'
+                    ]
+                },
+                {
+                    id: 5,
+                    question : "Which function is used to serialize an object into a JSON string?",
+                    options : [
+                        'stringify()',
+                        'parse()',
+                        'convert()',
+                    ]
+                }
+            ], 
+            answers : [0, 1, 2, 1, 0]
+    })
     res.json("succes posting")
     } catch (error) {
         res.json({error})
