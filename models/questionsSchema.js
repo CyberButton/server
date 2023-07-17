54591c32388d905ebfafe9fb24ec5de696781f34
CyberButton <87766623307Kk>
2023-07-17 18:07:30 +0600

    nado bystree eto uploadnut

--- models/questionsSchema.js
+++ models/questionsSchema.js
@@ -4,7 +4,10 @@ const { Schema } = mongoose
 const questionModel = new Schema({
     questions : {type : Array, default : []},
     answers : {type : Array, default : []},
-    createdAt : {type : Date, default : Date.now}
+    createdAt : {type : Date, default : Date.now},
+    userID : {type : String, default : ""},
+    nameOfMCQ : {type : String, default : ""},
+    numberOfMCQ : {type : Number, default : 0}
 })
 
 export default mongoose.model('Question', questionModel)
