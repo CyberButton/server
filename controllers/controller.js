2a815968e65102995ca1adeabc06cfb7dea8e8f2
CyberButton <87766623307Kk>
2023-07-21 11:19:35 +0600

    latest working

--- controllers/controller.js
+++ controllers/controller.js
@@ -109,70 +109,73 @@ export async function deleteResults(req, res) {
 
 export async function generateQuestions(req, res) {
     try {
-        // const configuration = new Configuration({
-        //     apiKey: process.env.OPENAI_API_KEY,
-        //     });
-        // const openai = new OpenAIApi(configuration);
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+            });
+        const openai = new OpenAIApi(configuration);
 
-        const { promt, numberOfMCQ, sourceType, userID, nameOfMCQ } = req.body
-        // console.log(userID)
-        // const chatCompletion = await openai.createChatCompletion({
-        //     model: "gpt-3.5-turbo",
-        //     messages: [{role: "system", content: `You are a proffesional MCQ(multiple choice question) generator. You make MCQs from the data given to you. To make those MCQs you only rely on the data provided to you even if its incorrect or misleading, your purpose is not to make totaly accurate MCQs but to make MCQs only from contetns of the data provided. You make questions and correct answer from provided data, while incorrect answer variants can be made up. You always respond in this exact format: 
-        //     {questions: [{id: 1,question : "question 1",options : ['Option0','Option1','Option2',]},{id: 2,question : "question 2",options : ['Option0','Option1','Option2',]}],answers : [1, 0]} , where answers array contains correct answers in this example option1 is the correct answer for q1 and option0 is the correct answer for q2.Finally you dont include any special symbols like /, \\, {, }, etc.`}, 
-        // {role: "user", content: `make me ${numberOfMCQ} MCQs from given ${sourceType}: "${promt}"`}],
-        //   });
+        const { prompt, numberOfMCQ, sourceType, userID, nameOfMCQ } = req.body
+
+        console.log({ prompt, numberOfMCQ, sourceType, userID, nameOfMCQ })
+
+        console.log(userID)
+        const chatCompletion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "system", content: `You are a proffesional MCQ(multiple choice question) generator. You make MCQs from the data given to you. To make those MCQs you only rely on the data provided to you even if its incorrect or misleading, your purpose is not to make totaly accurate MCQs but to make MCQs only from contetns of the data provided. You make questions and correct answer from provided data, while incorrect answer variants can be made up. You always respond in this exact format: 
+            {questions: [{id: 1,question : "question 1",options : ['Option0','Option1','Option2',]},{id: 2,question : "question 2",options : ['Option0','Option1','Option2',]}],answers : [1, 0]} , where answers array contains correct answers in this example option1 is the correct answer for q1 and option0 is the correct answer for q2.Finally you dont include any special symbols like /, \\, {, }, etc.`}, 
+        {role: "user", content: `make me ${numberOfMCQ} MCQs from given ${sourceType}: "${prompt}"`}],
+          });
               
         // Parse the API response string into a JSON object
         // const apiResponse = JSON.parse(chatCompletion.data.choices[0].message);
 
-        const data = `{
-            "questions": [
-              {
-                "id": 1,
-                "question": "DEBUG DEBUG DEGUB?",
-                "options": [
-                  "6.67 inches",
-                  "6.55 inches",
-                  "5.8 inches"
-                ]
-              },
-              {
-                "id": 2,
-                "question": "Which Gorilla Glass version is used for the front of Poco X3 Pro?",
-                "options": [
-                  "Gorilla Glass 6",
-                  "Gorilla Glass 5",
-                  "Gorilla Glass 3"
-                ]
-              },
-              {
-                "id": 3,
-                "question": "What is the RAM capacity of Poco X3 Pro's 256GB variant?",
-                "options": [
-                  "8GB",
-                  "6GB",
-                  "4GB"
-                ]
-              }
-            ],
-            "answers": [
-              0,
-              0,
-              0
-            ]
-          }`
+        // const data = `{
+        //     "questions": [
+        //       {
+        //         "id": 1,
+        //         "question": "who tf trynna  DEGUB?",
+        //         "options": [
+        //           "6.67 inches",
+        //           "6.55 inches",
+        //           "5.8 inches"
+        //         ]
+        //       },
+        //       {
+        //         "id": 2,
+        //         "question": "Which Gorilla Glass version is used for the front of Poco X3 Pro?",
+        //         "options": [
+        //           "Gorilla Glass 6",
+        //           "Gorilla Glass 5",
+        //           "Gorilla Glass 3"
+        //         ]
+        //       },
+        //       {
+        //         "id": 3,
+        //         "question": "What is the RAM capacity of Poco X3 Pro's 256GB variant?",
+        //         "options": [
+        //           "8GB",
+        //           "6GB",
+        //           "4GB"
+        //         ]
+        //       }
+        //     ],
+        //     "answers": [
+        //       0,
+        //       0,
+        //       0
+        //     ]
+        //   }`
         
-        //const data = chatCompletion.data.choices[0].message.content
+        const data = chatCompletion.data.choices[0].message.content
         const apiResponse = JSON.parse(data);
-        console.log(apiResponse)
+        // console.log(apiResponse)
 
         // Extract the questions and answers from the parsed JSON
         const { questions, answers } = apiResponse;
         // console.log(answers)
 
         Questions.create({questions, answers, userID, nameOfMCQ, numberOfMCQ})
-        res.json("questions saved succesfully")
+        res.json({questions, answers, userID, nameOfMCQ, numberOfMCQ})
           
     } catch (error) {
         console.log(error)
