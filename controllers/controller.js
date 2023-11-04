5c4ace52bc8f6371ba464ace3221e2fc076ad360
CyberButton <87766623307Kk>
2023-11-05 02:36:44 +0600

    rolling back to commit where rus sysmess was added

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,3 @@
-import { translateText } from "../google_translate.js";
 import Questions from "../models/questionsSchema.js"
 import Resluts from "../models/resultSchema.js"
 import { Configuration, OpenAIApi } from "openai"
@@ -8,24 +7,24 @@ export async function generatePDF(req, res) {
     const data = req.body;
     console.log(data)
     const doc = new PDFDocument();
-
+  
     // Create the PDF content here based on the 'data' received from the frontend
     doc.font('Helvetica-Bold').fontSize(20).text('Questionnaire', { align: 'center' });
-
+  
     data.questions.forEach((question, index) => {
-        doc.moveDown().font('Helvetica').fontSize(15).text(`${index + 1}. ${question.question}`);
-
-        question.options.forEach((option, optIndex) => {
-            const isCorrect = data.answers[index] === optIndex;
-            doc.moveDown().text(`${isCorrect ? '[X]' : '[ ]'} ${option}`);
-        });
+      doc.moveDown().font('Helvetica').fontSize(15).text(`${index + 1}. ${question.question}`);
+  
+      question.options.forEach((option, optIndex) => {
+        const isCorrect = data.answers[index] === optIndex;
+        doc.moveDown().text(`${isCorrect ? '[X]' : '[ ]'} ${option}`);
+      });
     });
-
+  
     // Pipe the PDF to the response
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=AIquiz.pdf');
     doc.pipe(res);
-    doc.end();
+    doc.end();  
 }
 
 export async function getQuestions(req, res) {
@@ -39,7 +38,7 @@ export async function getQuestions(req, res) {
 
 export async function postQuestions(req, res) {
     try {
-        Questions.insertMany({
+        Questions.insertMany({ 
             questions: [
                 {
                     id: 1,
@@ -86,10 +85,10 @@ export async function postQuestions(req, res) {
                         'convert()',
                     ]
                 }
-            ],
+            ], 
             answers: [0, 1, 2, 1, 0]
-        })
-        res.json("succes posting")
+    })
+    res.json("succes posting")
     } catch (error) {
         res.json({ error })
     }
@@ -97,14 +96,17 @@ export async function postQuestions(req, res) {
 
 export async function deleteQuestions(req, res) {
     try {
-        const quizId = req.params.quizId; // Get the quizId from req.params
-        await Questions.findByIdAndRemove(quizId);
-        res.json("questions delete function called successfully");
+      console.log("in delete");
+      console.log(req.params); // Use req.params instead of req.body to get the quizId
+      const quizId = req.params.quizId; // Get the quizId from req.params
+      console.log(quizId);
+      await Questions.findByIdAndRemove(quizId);
+      res.json("questions delete function called successfully");
     } catch (error) {
-        res.json({ error });
+      res.json({ error });
     }
-}
-
+  }
+  
 // for adimin stats
 export async function getAdminStats(req, res) {
     try {
@@ -141,7 +143,7 @@ export async function getResults(req, res) {
 export async function postResults(req, res) {
     try {
         const { username, results, attempts, nameOfMCQ, correct, achived } = req.body
-        if (!username || !results) {
+        if(!username || !results) {
             throw new Error('reslut useranme/result not provided')
         }
 
@@ -149,7 +151,7 @@ export async function postResults(req, res) {
         res.json("Result saved succesfully")
 
     } catch (error) {
-        res.json({ error })
+        res.json({error})
     }
 }
 
@@ -158,23 +160,32 @@ export async function deleteResults(req, res) {
         await Resluts.deleteMany()
         res.json("result deleted funcion called succesfully")
     } catch (error) {
-        res.json({ error })
+        res.json({error})
     }
 }
 
-export async function generateQuestions(req, res) {
+function formatQuestions(data) {
+    // Regular expression to find keys without double quotes
+    const regex = /("?\bquestions\b"?|"?\bid\b"?|"?\bquestion\b"?|"?\boptions\b"?|"?\banswers\b"?)/g;
 
+    // Replace keys without double quotes with keys with double quotes
+    const formattedData = data.replace(regex, '"$1"');
+    
+    return formattedData;
+  }
+
+export async function generateQuestions(req, res) {
+    
     try {
 
         const configuration = new Configuration({
             apiKey: process.env.OPENAI_API_KEY,
-        });
+            });
         const openai = new OpenAIApi(configuration);
 
         const { prompt, numberOfMCQ, sourceType, userID, nameOfMCQ, lang } = req.body
 
-        const fullText = [{
-            role: "system", content: `You are a proffesional MCQ(multiple choice question) generator. You make MCQs from the data given to you. To make those MCQs you only rely on the data provided to you even if its incorrect or misleading, your purpose is not to make totaly accurate MCQs but to make MCQs only from contetns of the data provided. You make questions and correct answer from provided data, while incorrect answer variants can be made up. You always respond in this json-like format { 
+        const fullText = [{role: "system", content: `You are a proffesional MCQ(multiple choice question) generator. You make MCQs from the data given to you. To make those MCQs you only rely on the data provided to you even if its incorrect or misleading, your purpose is not to make totaly accurate MCQs but to make MCQs only from contetns of the data provided. You make questions and correct answer from provided data, while incorrect answer variants can be made up. You always respond in this json-like format { 
             questions : [
                 {
                     id: _1_,
@@ -199,14 +210,11 @@ export async function generateQuestions(req, res) {
         _2_,
         _0_
         ]
-        }  where answers array contains indexes of correct answers in this example _option2_ is the correct answer for _question1_ and _option0_ is for _question 2_. Finally you dont include any special symbols/charachters try to replace them with text.`},
-        // {role: "user", content: `${lang === 'ru' ? ("Мне нужно "+ numberOfMCQ +" MCQ из заданных данных") : ("make me " + numberOfMCQ + " MCQs from given data")}: "${prompt}"`}]
-        { role: "user", content: `make me ${numberOfMCQ} MCQs from given data: "${prompt}"` }]
-
-
+        }  where answers array contains indexes of correct answers in this example _option2_ is the correct answer for _question1_ and _option0_ is for _question 2_. Finally you dont include any special symbols/charachters try to replace them with text.`}, 
+        {role: "user", content: `${lang === 'ru' ? ("Мне нужно "+ numberOfMCQ +" MCQ из заданных данных") : ("make me " + numberOfMCQ + " MCQs from given data")}: "${prompt}"`}]
+        
         const keyWords = [
-            {
-                role: "system", content: `You are a proffesional MCQ(multiple choice question) generator. You make fun, informative and engaging MCQs related the key words that will be given to you. You always respond in this json-like format { 
+            {role: "system", content: `You are a proffesional MCQ(multiple choice question) generator. You make fun, informative and engaging MCQs related the key words that will be given to you. You always respond in this json-like format { 
                 questions : [
                     {
                         id: _1_,
@@ -231,58 +239,70 @@ export async function generateQuestions(req, res) {
         _2_,
         _0_
         ]
-        }  where answers array contains indexes of correct answers in this example _option2_ is the correct answer for _question1_ and _option0_ is for _question 2_. Finally you dont include any special symbols/charachters try to replace them with text.`},
-            // {role: "user", content: `${lang === 'ru' ? ("Мне нужно " + numberOfMCQ + " MCQ из заданных ключевых слов") : ("make me " + numberOfMCQ + " MCQs from given key words")}: "${prompt}"`}
-
-            { role: "user", content: `make me ${numberOfMCQ} MCQs from given key words: "${prompt}"` }
-
-
+        }  where answers array contains indexes of correct answers in this example _option2_ is the correct answer for _question1_ and _option0_ is for _question 2_. Finally you dont include any special symbols/charachters try to replace them with text.`}, 
+        {role: "user", content: `${lang === 'ru' ? ("Мне нужно " + numberOfMCQ + " MCQ из заданных ключевых слов") : ("make me " + numberOfMCQ + " MCQs from given key words")}: "${prompt}"`}
         ]
-
+        
         const chatCompletion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: sourceType === "key words" ? keyWords : fullText,
-        });
-
+          });
+              
+        // const data = `{
+        //     "questions": [
+        //         {
+        //             "id": 1,
+        //             "question": "What is the screen size of Poco X3 Pro?",
+        //             "options": [
+        //                 "5.8 inches",
+        //                 "6.2 inches",
+        //                 "6.67 inches"
+        //             ]
+        //         },
+        //         {
+        //             "id": 2,
+        //             "question": "What is the rear camera resolution of Poco X3 Pro?",
+        //             "options": [
+        //                 "48 MP",
+        //                 "64 MP",
+        //                 "12 MP"
+        //             ]
+        //         },
+        //         {
+        //             "id": 3,
+        //             "question": "What is the battery capacity of Poco X3 Pro?",
+        //             "options": [
+        //                 "4000mAh",
+        //                 "4500mAh",
+        //                 "5160mAh"
+        //             ]
+        //         }
+        //     ],
+        //     "answers": [2, 0, 2]
+        // }`
+        
         let apiResponse;
 
         try {
             const data = chatCompletion.data.choices[0].message.content;
+            
+            console.log(data)
             apiResponse = JSON.parse(data);
         } catch (error) {
             console.error('Error parsing data:', error);
-            throw (error)
-        }
-
+            throw(error)
+        }  
+        
 
         // Extract the questions and answers from the parsed JSON
-        let { questions, answers } = apiResponse;
-        if (lang !== 'en') {
-            questions = await translateQuestionsArray(questions, "ru");
-        }
+        const { questions, answers } = apiResponse;
+        console.log( answers );
 
-        Questions.create({ questions, answers, userID, nameOfMCQ, numberOfMCQ })
-        res.json({ questions, answers, userID, nameOfMCQ, numberOfMCQ })
-
+        Questions.create({questions, answers, userID, nameOfMCQ, numberOfMCQ})
+        res.json({questions, answers, userID, nameOfMCQ, numberOfMCQ})
+          
     } catch (error) {
         console.log(error)
         res.status(500).send("pease try again")
     }
-}
-
-// Function to translate the questions array (asynchronous)
-async function translateQuestionsArray(questionsArray, target) {
-    const translatedQuestions = await Promise.all(questionsArray.map(async (question) => {
-        const translatedOptions = await Promise.all(question.options.map(async (option) => {
-            return await translateText(option, target);
-        }));
-
-        return {
-            ...question,
-            question: await translateText(question.question, target),
-            options: translatedOptions
-        };
-    }));
-
-    return translatedQuestions;
 }
